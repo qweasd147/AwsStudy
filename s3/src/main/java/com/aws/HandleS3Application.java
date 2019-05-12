9bb16fa89dc6c87381f3bac2ce0b6e993e8f2e1e
김주형 <qweasd_147@naver.com>
2019-05-12 22:06:13 +0900

    init spring boot project for s3

---
+++ s3/src/main/java/com/aws/HandleS3Application.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandleS3Application.class, args);
+    }
+
+}
